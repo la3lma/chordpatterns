1e27edb382a2a5e17bc27b1f148b788c51ab5ad4
Bjørn Remseth <rmz@comoyo.com>
2016-04-08 20:51:10 +0200

    Now handling c major scale2

---
+++ pattern-generator.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+pattern() {
+    local I=$1
+    local ii=$2
+    local iii=$3
+    local IV=$4
+    local V=$5
+    local vi=$6
+    local vii=$7
+    
+    
+cat <<EOF
+
+
+digraph G {
+
+  compound=true;
+  forcelabels=true;
+
+  iii[label="iii: $iii"];
+  vi[label="vi: $vi"];
+  V[label="V: $V"];
+  I[label="I: $I"];
+  IV[label="IV: $IV"];
+  vii[label="vii: $vii"];
+  ii[label="ii: $ii"];
+
+
+
+   subgraph cluster1 {
+      IV  -> ii;
+      color=blue
+   }
+   subgraph cluster2 {
+      vii;
+      V;
+   }
+   V -> vi [ltail=cluster2];
+   ii-> vii [ltail=cluster1, lhead=cluster2];
+   V   -> I [ltail=cluster2];
+   IV  -> I [ltail=cluster1];
+   iii -> vi;
+   vi  -> ii [lhead=cluster1];
+}
+EOF
+}
+
+
+pattern C d e F G a h
